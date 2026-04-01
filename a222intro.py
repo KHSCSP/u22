6ee8bbb74157564229055a62f2061892dbb49265
heyscottn <heyscottn@gmail.com>
2026-04-01 14:14:49 -0500

    mm

--- a222intro.py
+++ a222intro.py
@@ -1,6 +1,21 @@
+# here are the functions:
+#   ord() converts text to ascii number
+#   bin() converts integer to binary
+#   int(_, 2) converts binary to integer
+#   chr() converts integer to character
+
+
+
+
 name = "your name here"
 
 
 # TODO use ord() to show each ascii value
 #       use bin() to show each binary value
 #       use hex() to show each hex value
+
+
+
+
+# TODO read the 'secret.txt' file, decode!
+
