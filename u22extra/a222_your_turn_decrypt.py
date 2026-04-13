f4097e111a32c2b068483077eca849cac4452e43
heyscottn <heyscottn@gmail.com>
2026-04-13 14:35:36 -0500

    mm

--- u22extra/a222_your_turn_decrypt.py
+++
@@ -1,5 +0,0 @@
-# you've intercepted a secret message!
-# decrypt the text in the input file
-# you can copy the text to a list in this file
-# or import sys and use 'for line in sys.stdin', then run from the bash terminal
-
