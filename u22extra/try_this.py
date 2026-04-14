52bba8f6d49a4c5a96b4a6bf54864f84b001532c
heyscottn <heyscottn@gmail.com>
2026-04-14 14:05:46 -0500

    mm

--- u22extra/try_this.py
+++ u22extra/try_this.py
@@ -5,17 +5,5 @@
 #   chr() converts integer to character
 
 
-
-
-name = "your name here"
-
-
-# TODO use ord() to show each ascii value
-#       use bin() to show each binary value
-#       use hex() to show each hex value
-
-
-
-
 # TODO read the 'secret.txt' file, decode!
 
